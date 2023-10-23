cde2faf7ecf11e72d836d2e9e1c9f4d2c77ce872
George Davidson <georgedavidson392@gmail.com>
2023-10-22 20:41:08 -0400

    formatted

--- auto_clicker/main.cpp
+++ auto_clicker/main.cpp
@@ -1,12 +1,10 @@
+#include "key_codes.hpp"
 #include <Windows.h>
-
 #include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <thread>
-
-#include "key_codes.hpp"
 
 int leftKey = key_x;
 int leftClickDelay = 100;
@@ -22,67 +20,67 @@ bool terminateThread = false;
 
 void leftClicker(int leftKey, int leftClickDelay, int leftRandomOffset, int leftHoldTime)
 {
-	int leftAddedOffset = leftClickDelay + leftRandomOffset;
-	int leftSubtractedOffset = leftClickDelay - leftRandomOffset;
+    int leftAddedOffset = leftClickDelay + leftRandomOffset;
+    int leftSubtractedOffset = leftClickDelay - leftRandomOffset;
 
-	if (leftSubtractedOffset < 1)
-	{
-		leftSubtractedOffset = 1;
-	}
+    if (leftSubtractedOffset < 1)
+    {
+        leftSubtractedOffset = 1;
+    }
 
-	while (!terminateThread)
-	{
-		if (GetKeyState(leftKey))
-		{
-			int leftOffset = leftAddedOffset - leftSubtractedOffset;
-			int randomNumber = rand() % leftOffset + leftSubtractedOffset;
-			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-			Sleep(leftHoldTime);
-			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-			Sleep(randomNumber);
-		}
-	}
+    while (!terminateThread)
+    {
+        if (GetKeyState(leftKey))
+        {
+            int leftOffset = leftAddedOffset - leftSubtractedOffset;
+            int randomNumber = rand() % leftOffset + leftSubtractedOffset;
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            Sleep(leftHoldTime);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            Sleep(randomNumber);
+        }
+    }
 }
 
 void rightClicker(int rightKey, int rightClickDelay, int rightRandomOffset, int rightHoldTime)
 {
-	int rightAddedOffset = rightClickDelay + rightRandomOffset;
-	int rightSubtractedOffset = rightClickDelay - rightRandomOffset;
+    int rightAddedOffset = rightClickDelay + rightRandomOffset;
+    int rightSubtractedOffset = rightClickDelay - rightRandomOffset;
 
-	if (rightSubtractedOffset < 1)
-	{
-		rightSubtractedOffset = 1;
-	}
+    if (rightSubtractedOffset < 1)
+    {
+        rightSubtractedOffset = 1;
+    }
 
-	while (!terminateThread)
-	{
-		if (GetKeyState(rightKey))
-		{
-			int rightOffset = rightAddedOffset - rightSubtractedOffset;
-			int randomNumber = rand() % rightOffset + rightSubtractedOffset;
-			mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
-			Sleep(rightHoldTime);
-			mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
-			Sleep(randomNumber);
-		}
-	}
+    while (!terminateThread)
+    {
+        if (GetKeyState(rightKey))
+        {
+            int rightOffset = rightAddedOffset - rightSubtractedOffset;
+            int randomNumber = rand() % rightOffset + rightSubtractedOffset;
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+            Sleep(rightHoldTime);
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            Sleep(randomNumber);
+        }
+    }
 }
 
 int main()
 {
-	srand(time(0));
+    srand(time(0));
 
-	while (!terminateThread)
-	{
-		std::thread leftThread(leftClicker, leftKey, leftClickDelay, leftRandomOffset, leftHoldTime);
-		std::thread rightThread(rightClicker, rightKey, rightClickDelay, rightRandomOffset, rightHoldTime);
-		
-		std::cout << "press enter to exit...";
-		std::cin.ignore();
+    while (!terminateThread)
+    {
+        std::thread leftThread(leftClicker, leftKey, leftClickDelay, leftRandomOffset, leftHoldTime);
+        std::thread rightThread(rightClicker, rightKey, rightClickDelay, rightRandomOffset, rightHoldTime);
 
-		terminateThread = true;
-		leftThread.join();
-		rightThread.join();
-	}
-	return EXIT_SUCCESS;
+        std::cout << "press enter to exit...";
+        std::cin.ignore();
+
+        terminateThread = true;
+        leftThread.join();
+        rightThread.join();
+    }
+    return EXIT_SUCCESS;
 }
